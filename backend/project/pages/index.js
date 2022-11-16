78133d3b9e7b6178c54e2968d16b857c5d008cf2
paulAlexSerban <paul.alex.serban@gmail.com>
2022-11-17 01:06:03 +0200

    feat(next11): downgrade to next 11

--- backend/project/pages/index.js
+++ backend/project/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
+            href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
             <h2>Examples &rarr;</h2>
@@ -42,8 +42,6 @@ export default function Home() {
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
